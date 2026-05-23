e4d864992a1a90c85c17bd6d97ffe81ca13d772b
rian144hz <rianmatheus669@gmail.com>
2026-05-23 14:52:20 -0300

    Transações usando Statement, setAutoCommit, commit, rollBack

---
+++ src/application/Program4.java
@@ -0,0 +1,39 @@
+package application;
+
+import db.DB;
+import db.DbException;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Program4 {
+    public static void main(String[] args) throws SQLException {
+        Connection  conn = null;
+        Statement st = null;
+
+        try{
+            conn = DB.getConnection();
+            conn.setAutoCommit(false);
+            st = conn.createStatement();
+
+            int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
+//            int x = 1;
+//            if (x < 2){
+//                throw new SQLException("Fake error");
+//            }
+            int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+
+            conn.commit();
+
+            System.out.println("Rows1: "+rows1);
+            System.out.println("Rows2: "+rows2);
+        }catch (SQLException e){
+            conn.rollback();
+            throw  new DbException("Transaction rolled back! Caused by: "+e.getMessage());
+        }finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+    }
+}
